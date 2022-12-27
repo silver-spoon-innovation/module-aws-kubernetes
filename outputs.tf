e78211f158401dafaf49337c1cdab55f6ed9e9f9
pamornpolh <pamornpol.hanmatheekuna@gmail.com>
2022-12-27 15:09:49 +0700

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "eks_cluster_id" {
+	value = aws_eks_cluster.ms-sssm.id
+}
+
+output "eks_cluster_name" {
+	value = aws_eks_cluster.ms-sssm.name
+}
+
+output "eks_cluster_certificate_data" {
+	value = aws_eks_cluster.ms-sssm.certificate_authority[0].data
+}
+
+output "eks_cluster_endpoint" {
+	value = aws_eks_cluster.ms-sssm.endpoint
+}
+
+output "eks_cluster_nodegroup_id" {
+	value = aws_eks_node_group.ms-node-group.id
+}
